cb7dd01d81da61e1d8f31ad23ce955c5f29f0cf4
Ulrich Kemp <ulrich@kemp.in>
2024-06-25 23:50:03 +0200

    Added reading and writing of Subscribe

--- core/src/commonMain/kotlin/de/kempmobil/ktor/mqtt/RetainHandling.kt
+++ core/src/commonMain/kotlin/de/kempmobil/ktor/mqtt/RetainHandling.kt
@@ -8,4 +8,15 @@ public enum class RetainHandling(public val value: Int) {
     SEND_ON_SUBSCRIBE(0),
     SEND_IF_NOT_EXISTS(1),
     DO_NOT_SEND(2);
+
+    public companion object {
+        public fun from(value: Int): RetainHandling {
+            return when (value) {
+                0 -> SEND_ON_SUBSCRIBE
+                1 -> SEND_IF_NOT_EXISTS
+                2 -> DO_NOT_SEND
+                else -> throw MalformedPacketException("Unknown RetainHandling value: $value")
+            }
+        }
+    }
 }
--- core/src/commonMain/kotlin/de/kempmobil/ktor/mqtt/packet/Subscribe.kt
+++ core/src/commonMain/kotlin/de/kempmobil/ktor/mqtt/packet/Subscribe.kt
@@ -1,9 +1,9 @@
 package de.kempmobil.ktor.mqtt.packet
 
-import de.kempmobil.ktor.mqtt.SubscriptionIdentifier
-import de.kempmobil.ktor.mqtt.TopicFilter
-import de.kempmobil.ktor.mqtt.UserProperties
-import de.kempmobil.ktor.mqtt.wellFormedWhen
+import de.kempmobil.ktor.mqtt.*
+import de.kempmobil.ktor.mqtt.util.readMqttString
+import de.kempmobil.ktor.mqtt.util.writeMqttString
+import io.ktor.utils.io.core.*
 
 internal class Subscribe(
     val packetIdentifier: UShort,
@@ -17,4 +17,60 @@ internal class Subscribe(
     }
 
     override val headerFlags = 2
+}
+
+@OptIn(ExperimentalUnsignedTypes::class)
+internal fun BytePacketBuilder.write(subscribe: Subscribe) {
+    with(subscribe) {
+        writeUShort(subscribe.packetIdentifier)
+        writeProperties(
+            subscriptionIdentifier,
+            *userProperties.asArray
+        )
+
+        // Filter are written as payload
+        filters.forEach {
+            writeMqttString(it.filter)
+            writeByte(it.subscriptionOptions.bits)
+        }
+    }
+}
+
+@OptIn(ExperimentalUnsignedTypes::class)
+internal fun ByteReadPacket.readSubscribe(): Subscribe {
+    val packetIdentifier = readUShort()
+    val properties = readProperties()
+    val filters = buildList {
+        while (canRead()) {
+            val filter = readMqttString()
+            val options = readByte().toSubscriptionOptions()
+            add(TopicFilter(filter, options))
+        }
+    }
+
+    return Subscribe(
+        packetIdentifier = packetIdentifier,
+        filters = filters,
+        subscriptionIdentifier = properties.singleOrNull<SubscriptionIdentifier>(),
+        userProperties = UserProperties.from(properties)
+    )
+}
+
+private val SubscriptionOptions.bits: Byte
+    get() {
+        var bits = qoS.value
+        if (isNoLocal) bits = bits or 4
+        if (retainAsPublished) bits = bits or 8
+        bits = bits or (retainHandling.value shl 4)
+
+        return bits.toByte()
+    }
+
+private fun Byte.toSubscriptionOptions(): SubscriptionOptions {
+    val bits = toInt()
+    val qoS = QoS.from(bits and 3)
+    val isNoLocal = (bits and 4) shr 2 != 0
+    val retainAsPublished = (bits and 8) shr 3 != 0
+
+    return SubscriptionOptions(qoS, isNoLocal, retainAsPublished, RetainHandling.from((bits and 48) shr 4))
 }
