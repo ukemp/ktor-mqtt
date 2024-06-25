0a589751b19bd7d597fae95378ab5b264a9b1692
Ulrich Kemp <ulrich@kemp.in>
2024-06-25 21:00:48 +0200

    Implemented PUBACK, PUBREC, PUBREL and PUBCOMP

---
+++ core/src/commonMain/kotlin/de/kempmobil/ktor/mqtt/packet/PublishResponse.kt
@@ -0,0 +1,85 @@
+package de.kempmobil.ktor.mqtt.packet
+
+import de.kempmobil.ktor.mqtt.*
+import io.ktor.utils.io.core.*
+
+/**
+ * Base class for PUBACK, PUBREC, PUBREL and PUBCOMP.
+ */
+internal sealed class PublishResponse(
+    type: PacketType,
+    val packetIdentifier: UShort,
+    val reason: ReasonCode,
+    val reasonString: ReasonString? = null,
+    val userProperties: UserProperties = UserProperties.EMPTY
+) : AbstractPacket(type)
+
+internal class Puback(
+    packetIdentifier: UShort,
+    reason: ReasonCode,
+    reasonString: ReasonString? = null,
+    userProperties: UserProperties = UserProperties.EMPTY
+) : PublishResponse(PacketType.PUBACK, packetIdentifier, reason, reasonString, userProperties)
+
+internal class Pubrec(
+    packetIdentifier: UShort,
+    reason: ReasonCode,
+    reasonString: ReasonString? = null,
+    userProperties: UserProperties = UserProperties.EMPTY
+) : PublishResponse(PacketType.PUBACK, packetIdentifier, reason, reasonString, userProperties)
+
+internal class Pubrel(
+    packetIdentifier: UShort,
+    reason: ReasonCode,
+    reasonString: ReasonString? = null,
+    userProperties: UserProperties = UserProperties.EMPTY
+) : PublishResponse(PacketType.PUBACK, packetIdentifier, reason, reasonString, userProperties)
+
+internal class Pubcomp(
+    packetIdentifier: UShort,
+    reason: ReasonCode,
+    reasonString: ReasonString? = null,
+    userProperties: UserProperties = UserProperties.EMPTY
+) : PublishResponse(PacketType.PUBACK, packetIdentifier, reason, reasonString, userProperties)
+
+internal interface PublishResponseFactory<T> {
+
+    operator fun invoke(
+        packetIdentifier: UShort,
+        reason: ReasonCode,
+        reasonString: ReasonString? = null,
+        userProperties: UserProperties = UserProperties.EMPTY
+    ): T
+}
+
+@OptIn(ExperimentalUnsignedTypes::class)
+internal fun BytePacketBuilder.write(publishResponse: PublishResponse) {
+    with(publishResponse) {
+        writeUShort(packetIdentifier)
+        if (reason != Success) {
+            writeByte(reason.code.toByte())
+            writeProperties(
+                reasonString,
+                *userProperties.asArray
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalUnsignedTypes::class)
+internal fun <T> ByteReadPacket.readPublishResponse(createResponse: PublishResponseFactory<T>): T {
+    val packetIdentifier = readUShort()
+
+    return if (canRead()) {
+        val reason = ReasonCode.from(readByte())
+        val properties = readProperties()
+        createResponse(
+            packetIdentifier = packetIdentifier,
+            reason = reason,
+            reasonString = properties.singleOrNull<ReasonString>(),
+            userProperties = UserProperties.from(properties)
+        )
+    } else {
+        createResponse(packetIdentifier, Success)
+    }
+}
