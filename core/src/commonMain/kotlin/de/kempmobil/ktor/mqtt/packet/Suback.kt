212526b7c1ee2191cea4977eaa6af18fa4c92805
Ulrich Kemp <ulrich@kemp.in>
2024-06-27 15:07:30 +0200

    Added Suback

---
+++ core/src/commonMain/kotlin/de/kempmobil/ktor/mqtt/packet/Suback.kt
@@ -0,0 +1,41 @@
+package de.kempmobil.ktor.mqtt.packet
+
+import de.kempmobil.ktor.mqtt.*
+import io.ktor.utils.io.core.*
+
+internal data class Suback(
+    val reasonString: ReasonString? = null,
+    val userProperties: UserProperties = UserProperties.EMPTY,
+    val reasons: List<ReasonCode>
+) : AbstractPacket(PacketType.SUBACK) {
+
+    init {
+        wellFormedWhen(reasons.isNotEmpty()) { "Reason codes must not be empty in SUBACK" }
+    }
+}
+
+internal fun BytePacketBuilder.write(suback: Suback) {
+    with(suback) {
+        writeProperties(reasonString, *userProperties.asArray)
+
+        // Payload
+        reasons.forEach {
+            writeByte(it.code.toByte())
+        }
+    }
+}
+
+internal fun ByteReadPacket.readSuback(): Suback {
+    val properties = readProperties()
+    val reasons = buildList {
+        while (canRead()) {
+            add(ReasonCode.from(readByte(), defaultSuccessReason = GrantedQoS0))
+        }
+    }
+
+    return Suback(
+        reasonString = properties.singleOrNull<ReasonString>(),
+        userProperties = UserProperties.from(properties),
+        reasons = reasons
+    )
+}
