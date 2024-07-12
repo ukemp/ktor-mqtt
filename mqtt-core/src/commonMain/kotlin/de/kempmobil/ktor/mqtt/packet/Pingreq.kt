b3553a183603206fc4103d11d69a0a7dde10eac9
Ulrich Kemp <ulrich@kemp.in>
2024-07-12 22:41:54 +0200

    toString added to ping packets

--- mqtt-core/src/commonMain/kotlin/de/kempmobil/ktor/mqtt/packet/Pingreq.kt
+++ mqtt-core/src/commonMain/kotlin/de/kempmobil/ktor/mqtt/packet/Pingreq.kt
@@ -1,5 +1,10 @@
 package de.kempmobil.ktor.mqtt.packet
 
-public object Pingreq : AbstractPacket(PacketType.PINGREQ)
+public object Pingreq : AbstractPacket(PacketType.PINGREQ) {
+
+    override fun toString(): String {
+        return "Pingreq"
+    }
+}
 
 // PINGREQ consists only of the fixed header, hence, nothing else to do here!
--- mqtt-core/src/commonMain/kotlin/de/kempmobil/ktor/mqtt/packet/Pingresp.kt
+++ mqtt-core/src/commonMain/kotlin/de/kempmobil/ktor/mqtt/packet/Pingresp.kt
@@ -1,5 +1,10 @@
 package de.kempmobil.ktor.mqtt.packet
 
-public object Pingresp : AbstractPacket(PacketType.PINGRESP)
+public object Pingresp : AbstractPacket(PacketType.PINGRESP) {
+
+    override fun toString(): String {
+        return "Pingesp"
+    }
+}
 
 // PINGRESP consists only of the fixed header, hence, nothing else to do here!
