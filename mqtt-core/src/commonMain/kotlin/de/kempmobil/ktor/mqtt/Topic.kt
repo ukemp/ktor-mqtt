d90ae68c17cd1202ed255e9a7ea84b826f6ff8b9
Ulrich Kemp <ulrich@kemp.in>
2026-01-27 19:10:14 +0100

    Added shared subscription detection for topics

--- mqtt-core/src/commonMain/kotlin/de/kempmobil/ktor/mqtt/Topic.kt
+++ mqtt-core/src/commonMain/kotlin/de/kempmobil/ktor/mqtt/Topic.kt
@@ -17,10 +17,37 @@ public value class Topic(public val name: String) {
         return name.indexOfAny(charArrayOf('#', '+')) != -1
     }
 
+    /**
+     * Determines whether this topic is a valid 'shared subscription name', hence has the form:
+     * `$share/{ShareName}/{filter}`
+     */
+    public fun isShared(): Boolean {
+        return shareRegex.matches(name)
+    }
+
+    /**
+     * When this is a shared subscription (see [isShared]), returns the name of the share and the remaining filter,
+     * otherwise throws [IllegalStateException]. Hence, for a topic `$share/consumer1/sport/tennis/+` this method
+     * returns `consumer1` as the share name and `sport/tennis/+` as the filter.
+     */
+    public fun shareNameAndFilter(): Pair<String, Topic> {
+        val m = shareRegex.find(name)
+        if (m != null) {
+            return m.groupValues[1] to Topic(m.groupValues[2])
+        } else {
+            throw IllegalStateException("'$name' is not a valid shared subscription")
+        }
+    }
+
     public fun isNotBlank(): Boolean = name.isNotBlank()
 
     override fun toString(): String {
         return name
+    }
+
+    internal companion object {
+
+        val shareRegex = Regex($$"""\$share/([^+#/]+)/(.+)""")
     }
 }
 
